88d36f45c161956c7a45a96e85da85852e5e0b2c
Victor <zz_xxba-x@mail.ru>
2023-05-29 15:12:40 +0300

    задача 10

---
+++ task00/Program.cs
@@ -0,0 +1,26 @@
+﻿void FindLargestDigit(int num)
+{
+    int firstNumber = num / 10;
+    int secondNumber = num % 10;
+    Console.WriteLine("Рандомное число: " + num);
+    Console.WriteLine(firstNumber);
+    Console.WriteLine(secondNumber);
+    if (firstNumber > secondNumber)
+    {
+        Console.WriteLine("Первая цифра больше второй");
+    }
+    else if (secondNumber > firstNumber)
+    {
+        Console.WriteLine("Вторая цифра больше первой");
+    }
+    else
+    {
+        Console.WriteLine("Цифры равны");
+    }
+}
+int number = new Random().Next(10, 100);
+int number1 = new Random().Next(100);
+int number2 = new Random().Next(-80, 10);
+FindLargestDigit(number);
+FindLargestDigit(number1);
+FindLargestDigit(number2);
---
+++ task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int FindFirstDigit(int num)
+{
+    int firstNumber = num / 100;
+    return firstNumber;
+}
+int FindSecondDigit(int num)
+{
+    int secondNumber = num % 10;
+    return secondNumber;
+}
+
+void E(int num, int firstNumber, int secondNumber)
+{
+    Console.WriteLine("Случайное число "+ num);
+    Console.Write(firstNumber);
+    Console.Write(secondNumber);
+}
+
+int number = new Random().Next(100, 1000);
+E(number, FindFirstDigit(number), FindSecondDigit(number));
